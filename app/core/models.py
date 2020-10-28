1c13b303fce40fae268195e81dbfbc997b60f362
jorgemaciel <jorge.maciel.jr@gmail.com>
2020-10-28 16:47:17 -0300

    Added endpoint for creating and authentication users

--- app/core/models.py
+++ app/core/models.py
@@ -12,16 +12,14 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-        
         return user
-    
+
     def create_superuser(self, email, password):
         """Creates and saves a new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
 
 
@@ -31,13 +29,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-    
-
-
-
-
-
